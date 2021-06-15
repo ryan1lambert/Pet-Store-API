460efd0d0071141e9754e6af95da19ae6fd1a99e
rlambert <rlambert@172.19.6.77>
2021-06-15 14:09:21 -0400

    Petstore API complete

--- src/main/java/petStore/pet/PetController.java
+++ src/main/java/petStore/pet/PetController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,4 +31,32 @@ public class PetController {
 		petService.addPet(pet);
 	}
 	
+	@RequestMapping(method=RequestMethod.PUT, value="/pets/{id}")
+	public void updatePet(@RequestBody Pet pet, @PathVariable int id) {
+		petService.updatePet(id, pet);
+	}
+	
+	@RequestMapping(value="/pets/findByTags")
+	public List<Pet> findByTags(@RequestParam(value="tag") String[] tags ) {
+		return petService.getByTag(tags);
+	}
+	
+	@RequestMapping(value="/pets/findByStatus")
+	public List<Pet> findByStatus(@RequestParam(value="status") String status){
+		return petService.getByStatus(status);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/pets/{id}/uploadImage")
+	public void uploadImage(@PathVariable int id, @RequestParam(value="img") String img) {
+		petService.addImage(id, img);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/pets/{id}")
+	public void updateNameAndStatus(@PathVariable int id, @RequestParam(value="name") String name, @RequestParam(value="status") String status) {
+		petService.updateNameAndStatus(id, name, status);
+	}
+	@RequestMapping(method = RequestMethod.DELETE, value="/pets/{id}")
+	public void deletePet(@PathVariable int id) {
+		petService.deletePet(id);
+	}
 }
--- src/main/java/petStore/pet/PetService.java
+++ src/main/java/petStore/pet/PetService.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Service;
 public class PetService {
 	//this class is where the data is instantiated
 	private List<Pet> pets = new ArrayList<>(Arrays.asList(
-			new Pet(0,"name", "category", "photourl", Arrays.asList("tag1", "tag2"), "status" ),
-			new Pet(1, "Mo", "Dog", "photourl", Arrays.asList("bad", "small"), "available" ),
-			new Pet(2, "Cooper", "Dog", "photourl", Arrays.asList("good", "big"), "not available" )
+			new Pet(0,"name", "category", "photourl", new ArrayList<>(Arrays.asList("tag1", "tag2")), "status" ),
+			new Pet(1, "Mo", "Dog", "photourl", new ArrayList<>(Arrays.asList("bad", "small")), "available" ),
+			new Pet(2, "Cooper", "Dog", "photourl",new ArrayList<>(Arrays.asList("good", "big")), "not available" )
 			));
 	public List<Pet> getAllPets(){
 		return pets;
@@ -22,6 +22,63 @@ public class PetService {
 	}
 	public void addPet(Pet pet) {
 		pets.add(pet);
+	}
+	public List<Pet> getByTag(String[] tags) {
+		List<Pet> result = new ArrayList<>();
+		for(int i = 0; i < pets.size(); i++) {
+			for(int j = 0; j < tags.length; j++) {
+				if(pets.get(i).getTags().contains(tags[j])) {
+					result.add(pets.get(i));
+				}
+			}
+		}
+		return result;
+	}
+	public void updatePet(int id, Pet pet) {
+		for(int i=0; i < pets.size(); i++) {
+			if(pets.get(i).getId()==id) {
+				pets.set(i, pet);
+				return;
+			}
+		}
+	}
+	
+	public void addImage(int id, String img) {
+		for(int i=0; i < pets.size(); i++) {
+			if(pets.get(i).getId()==id) {
+				pets.get(i).setPhotoUrl(img);
+				return;
+			}
+		}
+	}
+	
+	public List<Pet> getByStatus(String status){
+		List<Pet> result = new ArrayList<>();
+		for(int i=0; i < pets.size(); i++) {
+			if(pets.get(i).getStatus().equals(status)) {
+				result.add(pets.get(i));
+			}
+		}
+		return result; 
+	}
+	
+	public void updateNameAndStatus(int id, String name, String status) {
+		for(int i=0; i < pets.size(); i++) {
+			if(pets.get(i).getId()==id) {
+				pets.get(i).setName(name);
+				pets.get(i).setStatus(status);
+				return;
+			}
+		}
+	}
+	
+	public void deletePet(int id) {
+		for(int i=0; i < pets.size(); i++) {
+			if(pets.get(i).getId()==id) {
+				pets.remove(i);
+				return;
+			}
+		}
 	}
 
 }
